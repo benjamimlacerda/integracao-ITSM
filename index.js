aa9d18766ddec41bbdcff07dbcbc4c5400397e20
Benjamim Lacerda <benjamim.lacerda16@gmail.com>
2025-11-05 11:23:52 -0300

    Retira o resend como dependencia

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const https = require("https");
-const { Resend } = require("resend");
 
 
 const app = express();
